9043be25ad17fc114fc06f6277546a1a810b6a9a
Nikita Sirovskiy <nicksirovsky@gmail.com>
2023-01-31 09:32:12 +0300

    Correct the name

--- lib/slidable_tab_bar_data.dart
+++ lib/slidable_tab_bar_data.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/widgets.dart';
 
-class SlideableTabData {
-  const SlideableTabData(this.title, this.child);
+class SlidableTabData {
+  const SlidableTabData(this.title, this.child);
 
   final String title;
   final Widget child;
